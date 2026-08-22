5ce72369858d4aa07a42ec20f7c806687a5c0c85
UdayKumar <Udaykumar@ozrit.in>
2026-08-22 13:11:09 +0530

    feat: increase express request body size limits to 100mb

--- app.js
+++ app.js
@@ -34,8 +34,8 @@ app.use(cors({
   credentials: true,
 }));
 app.use(morgan('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
